63d7b2efcdb6b09d13f2b316a3f5548f894f97b0
wenzouquan <359945126@qq.com>
2017-12-03 17:05:49 +0800

    12

--- pages/index/issue.js
+++ pages/index/issue.js
@@ -6,13 +6,40 @@ Page({
   data: {
     userInfo: {},
     address:'',
+    date:'',
+    maxPeople:'',
+    minPeople:'',
+    advanceDateText:'',
+    discountText:'',
     imageList:[],
     topicType:2,
     markers: [
     ],
     expenseArray: ['0〜100元', '100〜200元', '200〜500元','500元以上'],
     genderArray:['女','男','男女均可'],
-    numberOfPeople: ['1', '1〜3', '3〜5', '5〜8','8人以上'],
+    numbers: ['1', '2', '3', '4','5','6','7','8','9'],
+    discounts: ['5折', '6折', '7折', '8折', '9折'],
+  },
+  //设置人数
+  bindPickerChange:function(e){
+    var v = Number(e.detail.value)+1;
+    if (e.currentTarget.dataset.type=="min"){
+      this.setData({ minPeople:v});
+    } else if (e.currentTarget.dataset.type == "max"){
+      this.setData({ advanceDate: v, advanceDateText:v+'天' });
+    }else{
+
+    }
+  },
+  //设置时间
+  bindDateChange:function(e){
+    var v = e.detail.value;
+    this.setData({ date: v });
+  },
+  //打折
+  bindDiscountsChange:function(e){
+    var v = e.detail.value;
+    this.setData({ discount:v, discountText: this.data.discounts[v] });
   },
   onLoad: function (options){
     this.setData({ topicType: options.topicType});
--- pages/user/money.js
+++ pages/user/money.js
@@ -5,15 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-    type:3
+    type:3,
+    showAddBank: 1,
+    array: ['招商银行', '北京银行'],
+    bankname: '',
+  },
+  bindPickerChange: function (e) {
+    this.setData({ bankname: this.data.array[e.detail.value] });
+    console.log(e);
+  },
+  doAddBank:function(){
+    this.setData({ showAddBank: 0 });
+  },
+  selectBank: function(){
+    this.setViweType(4);
+  },
+  doWithDraw:function(){
+    this.setViweType(0);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({ type: options.type});
+    
     console.log(options);
+  },
+  showView:function(e){
+    this.setViweType(e.currentTarget.dataset.id );
+    console.log(e.currentTarget.dataset.id);
+  },
+  setViweType:function(id){
+    this.setData({ type: id });
+    var title ="我的总收入";
+    if(id==2){
+      title="未结算的金额";
+    }else if(id==3){
+      title = "可提现的金额";
+    }else if(id ==4){
+      title = "提现到银行卡";
+    }else if(id == 5){
+      title = "银行卡管理";
+    }
+    wx.setNavigationBarTitle({
+      title: title
+    })
+  },
+  addBank: function (e) {
+    console.log(e);
+    this.setData({ showAddBank: e.currentTarget.dataset.id });
   },
 
   /**
